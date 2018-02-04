1079bab595d5ab2112ec1739e7749e18c983d9cc
YZhenY <fuz3zy@gmail.com>
2018-02-03 18:27:20 -0800

    added initial files

---
+++ public/src/components/App.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentForm: this.props.forms[0],
+      forms: this.props.forms;
+      selectedForm: this.props.forms[0]
+      response: '',
+    }
+
+    this.url = ''//link to Docusign API.
+  }
+
+  //makes calls the Docusign API for calls.
+  requestSignature() {
+
+  }
+
+  //Stores signed documents
+  storeSignatures() {
+
+  }
+
+
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return(
+      <div className="App">
+        <NavBar />
+        <div className="row container">
+          <div className="col-md-6 offset-md-3"
+            <Inputs />
+          </div>
+        <div>
+        <br/>
+          API Call example:
+        <br/>
+          {this.state.response}
+      </div>
+    );
+  }
+}
+
+window.App = App;
+export default App;
---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+var port = process.env.PORT || 3000;
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(express.static('public'))
+
+app.listen(port, () => console.log('Example app listening on port' + port))
